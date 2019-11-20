a613491f4e002dd64b5013642d870667aec52c9e
hejiawen <hjw0415@utlook.com>
2019-11-20 09:43:25 +0800

    使用H743作为模板，新建H743_V版本芯片例程

---
+++ H743_Pro_V版本芯片/22-1，任务通知代替消息队列/User/main.c
@@ -0,0 +1,440 @@
+/**
+  *********************************************************************
+  * @file    main.c
+  * @author  fire
+  * @version V1.0
+  * @date    2018-xx-xx
+  * @brief   FreeRTOS V9.0.0  + STM32 任务通知代替消息队列
+  *********************************************************************
+  * @attention
+  *
+  * 实验平台:野火 STM32全系列开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :https://fire-stm32.taobao.com
+  *
+  **********************************************************************
+  */ 
+ 
+/*
+*************************************************************************
+*                             包含的头文件
+*************************************************************************
+*/ 
+/* FreeRTOS头文件 */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "limits.h"
+/* 开发板硬件bsp头文件 */
+#include "stm32h7xx.h"
+#include "main.h"
+#include "bsp_led.h"
+#include "bsp_key.h" 
+#include "core_delay.h"   
+#include "bsp_debug_usart.h"
+
+/**************************** 任务句柄 ********************************/
+/* 
+ * 任务句柄是一个指针，用于指向一个任务，当任务创建好之后，它就具有了一个任务句柄
+ * 以后我们要想操作这个任务都需要通过这个任务句柄，如果是自身的任务操作自己，那么
+ * 这个句柄可以为NULL。
+ */
+static TaskHandle_t AppTaskCreate_Handle = NULL;/* 创建任务句柄 */
+static TaskHandle_t Receive1_Task_Handle = NULL;/* Receive1_Task任务句柄 */
+static TaskHandle_t Receive2_Task_Handle = NULL;/* Receive2_Task任务句柄 */
+static TaskHandle_t Send_Task_Handle = NULL;/* Send_Task任务句柄 */
+
+/********************************** 内核对象句柄 *********************************/
+/*
+ * 信号量，消息队列，事件标志组，软件定时器这些都属于内核的对象，要想使用这些内核
+ * 对象，必须先创建，创建成功之后会返回一个相应的句柄。实际上就是一个指针，后续我
+ * 们就可以通过这个句柄操作这些内核对象。
+ *
+ * 内核对象说白了就是一种全局的数据结构，通过这些数据结构我们可以实现任务间的通信，
+ * 任务间的事件同步等各种功能。至于这些功能的实现我们是通过调用这些内核对象的函数
+ * 来完成的
+ * 
+ */
+
+
+/******************************* 全局变量声明 ************************************/
+/*
+ * 当我们在写应用程序的时候，可能需要用到一些全局变量。
+ */
+
+
+/******************************* 宏定义 ************************************/
+/*
+ * 当我们在写应用程序的时候，可能需要用到一些宏定义。
+ */
+#define  USE_CHAR  0  /* 测试字符串的时候配置为 1 ，测试变量配置为 0  */
+
+/*
+*************************************************************************
+*                             函数声明
+*************************************************************************
+*/
+static void AppTaskCreate(void);/* 用于创建任务 */
+
+static void Receive1_Task(void* pvParameters);/* Receive1_Task任务实现 */
+static void Receive2_Task(void* pvParameters);/* Receive2_Task任务实现 */
+
+static void Send_Task(void* pvParameters);/* Send_Task任务实现 */
+
+static void BSP_Init(void);/* 用于初始化板载相关资源 */
+
+/*****************************************************************
+  * @brief  主函数
+  * @param  无
+  * @retval 无
+  * @note   第一步：开发板硬件初始化 
+            第二步：创建APP应用任务
+            第三步：启动FreeRTOS，开始多任务调度
+  ****************************************************************/
+int main(void)
+{	
+  BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+  
+  /* 开发板硬件初始化 */
+  BSP_Init();
+	printf("这是一个[野火]-STM32全系列开发板-FreeRTOS任务通知代替消息队列实验！\n");
+  printf("按下KEY1或者KEY2进行任务消息通知发送 \n");
+   /* 创建AppTaskCreate任务 */
+  xReturn = xTaskCreate((TaskFunction_t )AppTaskCreate,  /* 任务入口函数 */
+                        (const char*    )"AppTaskCreate",/* 任务名字 */
+                        (uint16_t       )512,  /* 任务栈大小 */
+                        (void*          )NULL,/* 任务入口函数参数 */
+                        (UBaseType_t    )1, /* 任务的优先级 */
+                        (TaskHandle_t*  )&AppTaskCreate_Handle);/* 任务控制块指针 */ 
+  /* 启动任务调度 */           
+  if(pdPASS == xReturn)
+    vTaskStartScheduler();   /* 启动任务，开启调度 */
+  else
+    return -1;  
+  
+  while(1);   /* 正常不会执行到这里 */    
+}
+
+
+/***********************************************************************
+  * @ 函数名  ： AppTaskCreate
+  * @ 功能说明： 为了方便管理，所有的任务创建函数都放在这个函数里面
+  * @ 参数    ： 无  
+  * @ 返回值  ： 无
+  **********************************************************************/
+static void AppTaskCreate(void)
+{
+  BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+  
+  taskENTER_CRITICAL();           //进入临界区
+
+  /* 创建Receive1_Task任务 */
+  xReturn = xTaskCreate((TaskFunction_t )Receive1_Task, /* 任务入口函数 */
+                        (const char*    )"Receive1_Task",/* 任务名字 */
+                        (uint16_t       )512,   /* 任务栈大小 */
+                        (void*          )NULL,	/* 任务入口函数参数 */
+                        (UBaseType_t    )2,	    /* 任务的优先级 */
+                        (TaskHandle_t*  )&Receive1_Task_Handle);/* 任务控制块指针 */
+  if(pdPASS == xReturn)
+    printf("创建Receive1_Task任务成功!\r\n");
+  
+  /* 创建Receive2_Task任务 */
+  xReturn = xTaskCreate((TaskFunction_t )Receive2_Task, /* 任务入口函数 */
+                        (const char*    )"Receive2_Task",/* 任务名字 */
+                        (uint16_t       )512,   /* 任务栈大小 */
+                        (void*          )NULL,	/* 任务入口函数参数 */
+                        (UBaseType_t    )3,	    /* 任务的优先级 */
+                        (TaskHandle_t*  )&Receive2_Task_Handle);/* 任务控制块指针 */
+  if(pdPASS == xReturn)
+    printf("创建Receive2_Task任务成功!\r\n");
+  
+  /* 创建Send_Task任务 */
+  xReturn = xTaskCreate((TaskFunction_t )Send_Task,  /* 任务入口函数 */
+                        (const char*    )"Send_Task",/* 任务名字 */
+                        (uint16_t       )512,  /* 任务栈大小 */
+                        (void*          )NULL,/* 任务入口函数参数 */
+                        (UBaseType_t    )4, /* 任务的优先级 */
+                        (TaskHandle_t*  )&Send_Task_Handle);/* 任务控制块指针 */ 
+  if(pdPASS == xReturn)
+    printf("创建Send_Task任务成功!\n\n");
+  
+  vTaskDelete(AppTaskCreate_Handle); //删除AppTaskCreate任务
+  
+  taskEXIT_CRITICAL();            //退出临界区
+}
+
+
+
+/**********************************************************************
+  * @ 函数名  ： Receive_Task
+  * @ 功能说明： Receive_Task任务主体
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  ********************************************************************/
+static void Receive1_Task(void* parameter)
+{	
+  BaseType_t xReturn = pdTRUE;/* 定义一个创建信息返回值，默认为pdPASS */
+#if USE_CHAR
+  char *r_char;
+#else
+  uint32_t r_num;
+#endif
+  while (1)
+  {
+    /* BaseType_t xTaskNotifyWait(uint32_t ulBitsToClearOnEntry, 
+                                  uint32_t ulBitsToClearOnExit, 
+                                  uint32_t *pulNotificationValue, 
+                                  TickType_t xTicksToWait ); 
+     * ulBitsToClearOnEntry：当没有接收到任务通知的时候将任务通知值与此参数的取
+       反值进行按位与运算，当此参数为Oxfffff或者ULONG_MAX的时候就会将任务通知值清零。
+     * ulBits ToClearOnExit：如果接收到了任务通知，在做完相应的处理退出函数之前将
+       任务通知值与此参数的取反值进行按位与运算，当此参数为0xfffff或者ULONG MAX的时候
+       就会将任务通知值清零。
+     * pulNotification Value：此参数用来保存任务通知值。
+     * xTick ToWait：阻塞时间。
+     *
+     * 返回值：pdTRUE：获取到了任务通知。pdFALSE：任务通知获取失败。
+     */
+    //获取任务通知 ,没获取到则一直等待
+		xReturn=xTaskNotifyWait(0x0,			//进入函数的时候不清除任务bit
+                            ULONG_MAX,	  //退出函数的时候清除所有的bit
+#if USE_CHAR
+                            (uint32_t *)&r_char,		  //保存任务通知值
+#else
+                            &r_num,		  //保存任务通知值
+#endif                        
+                            portMAX_DELAY);	//阻塞时间
+    if( pdTRUE == xReturn )
+#if USE_CHAR
+      printf("Receive1_Task 任务通知消息为 %s \n",r_char);                      
+#else
+      printf("Receive1_Task 任务通知消息为 %d \n",r_num);                      
+#endif  
+     
+    
+		LED1_TOGGLE;
+  }
+}
+
+/**********************************************************************
+  * @ 函数名  ： Receive_Task
+  * @ 功能说明： Receive_Task任务主体
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  ********************************************************************/
+static void Receive2_Task(void* parameter)
+{	
+  BaseType_t xReturn = pdTRUE;/* 定义一个创建信息返回值，默认为pdPASS */
+#if USE_CHAR
+  char *r_char;
+#else
+  uint32_t r_num;
+#endif
+  while (1)
+  {
+    /* BaseType_t xTaskNotifyWait(uint32_t ulBitsToClearOnEntry, 
+                                  uint32_t ulBitsToClearOnExit, 
+                                  uint32_t *pulNotificationValue, 
+                                  TickType_t xTicksToWait ); 
+     * ulBitsToClearOnEntry：当没有接收到任务通知的时候将任务通知值与此参数的取
+       反值进行按位与运算，当此参数为Oxfffff或者ULONG_MAX的时候就会将任务通知值清零。
+     * ulBits ToClearOnExit：如果接收到了任务通知，在做完相应的处理退出函数之前将
+       任务通知值与此参数的取反值进行按位与运算，当此参数为0xfffff或者ULONG MAX的时候
+       就会将任务通知值清零。
+     * pulNotification Value：此参数用来保存任务通知值。
+     * xTick ToWait：阻塞时间。
+     *
+     * 返回值：pdTRUE：获取到了任务通知。pdFALSE：任务通知获取失败。
+     */
+    //获取任务通知 ,没获取到则一直等待
+		xReturn=xTaskNotifyWait(0x0,			//进入函数的时候不清除任务bit
+                            ULONG_MAX,	  //退出函数的时候清除所有的bit
+#if USE_CHAR
+                            (uint32_t *)&r_char,		  //保存任务通知值
+#else
+                            &r_num,		  //保存任务通知值
+#endif                        
+                            portMAX_DELAY);	//阻塞时间
+    if( pdTRUE == xReturn )
+#if USE_CHAR
+      printf("Receive2_Task 任务通知消息为 %s \n",r_char);                      
+#else
+      printf("Receive2_Task 任务通知消息为 %d \n",r_num);                      
+#endif  
+		LED2_TOGGLE;
+  }
+}
+
+/**********************************************************************
+  * @ 函数名  ： Send_Task
+  * @ 功能说明： Send_Task任务主体
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  ********************************************************************/
+static void Send_Task(void* parameter)
+{	 
+  BaseType_t xReturn = pdPASS;/* 定义一个创建信息返回值，默认为pdPASS */
+#if USE_CHAR
+  char test_str1[] = "this is a mail test 1";/* 邮箱消息test1 */
+  char test_str2[] = "this is a mail test 2";/* 邮箱消息test2 */
+#else
+  uint32_t send1 = 1;
+  uint32_t send2 = 2;
+#endif
+  
+
+  
+  while (1)
+  {
+    /* KEY1 被按下 */
+    if( Key_Scan(KEY1_GPIO_PORT,KEY1_PIN) == KEY_ON )
+    {
+      /* 原型:BaseType_t xTaskNotify( TaskHandle_t xTaskToNotify, 
+                                      uint32_t ulValue, 
+                                      eNotifyAction eAction ); 
+       * eNoAction = 0，通知任务而不更新其通知值。
+       * eSetBits，     设置任务通知值中的位。
+       * eIncrement，   增加任务的通知值。
+       * eSetvaluewithoverwrite，覆盖当前通知
+       * eSetValueWithoutoverwrite 不覆盖当前通知
+       * 
+       * pdFAIL：当参数eAction设置为eSetValueWithoutOverwrite的时候，
+       * 如果任务通知值没有更新成功就返回pdFAIL。
+       * pdPASS: eAction 设置为其他选项的时候统一返回pdPASS。
+      */
+      xReturn = xTaskNotify( Receive1_Task_Handle, /*任务句柄*/
+#if USE_CHAR 
+                             (uint32_t)&test_str1, /* 发送的数据，最大为4字节 */
+#else
+                              send1, /* 发送的数据，最大为4字节 */
+#endif
+                             eSetValueWithOverwrite );/*覆盖当前通知*/
+      
+      if( xReturn == pdPASS )
+        printf("Receive1_Task_Handle 任务通知消息发送成功!\r\n");
+    } 
+    /* KEY2 被按下 */
+    if( Key_Scan(KEY2_GPIO_PORT,KEY2_PIN) == KEY_ON )
+    {
+      xReturn = xTaskNotify( Receive2_Task_Handle, /*任务句柄*/
+#if USE_CHAR 
+                             (uint32_t)&test_str2, /* 发送的数据，最大为4字节 */
+#else
+                              send2, /* 发送的数据，最大为4字节 */
+#endif
+                             eSetValueWithOverwrite );/*覆盖当前通知*/
+      /* 此函数只会返回pdPASS */
+      if( xReturn == pdPASS )
+        printf("Receive2_Task_Handle 任务通知消息发送成功!\r\n");
+    }
+    vTaskDelay(20);
+  }
+}
+
+
+/***********************************************************************
+  * @ 函数名  ： BSP_Init
+  * @ 功能说明： 板级外设初始化，所有板子上的初始化均可放在这个函数里面
+  * @ 参数    ：   
+  * @ 返回值  ： 无
+  *********************************************************************/
+static void BSP_Init(void)
+{
+  /* 系统时钟初始化成400MHz */
+	SystemClock_Config();
+  
+  /* 初始化SysTick */
+  HAL_SYSTICK_Config( HAL_RCC_GetSysClockFreq() / configTICK_RATE_HZ );	
+
+	/* 硬件BSP初始化统统放在这里，比如LED，串口，LCD等 */
+    
+	/* LED 端口初始化 */
+	LED_GPIO_Config();	
+	
+	/* usart 端口初始化 */
+  DEBUG_USART_Config();
+
+  /* KEY 端口初始化 */
+  Key_GPIO_Config();
+  
+}
+ 
+/**
+  * @brief  System Clock 配置
+  *         system Clock 配置如下: 
+	*            System Clock source  = PLL (HSE)
+	*            SYSCLK(Hz)           = 400000000 (CPU Clock)
+	*            HCLK(Hz)             = 200000000 (AXI and AHBs Clock)
+	*            AHB Prescaler        = 2
+	*            D1 APB3 Prescaler    = 2 (APB3 Clock  100MHz)
+	*            D2 APB1 Prescaler    = 2 (APB1 Clock  100MHz)
+	*            D2 APB2 Prescaler    = 2 (APB2 Clock  100MHz)
+	*            D3 APB4 Prescaler    = 2 (APB4 Clock  100MHz)
+	*            HSE Frequency(Hz)    = 25000000
+	*            PLL_M                = 5
+	*            PLL_N                = 160
+	*            PLL_P                = 2
+	*            PLL_Q                = 4
+	*            PLL_R                = 2
+	*            VDD(V)               = 3.3
+	*            Flash Latency(WS)    = 4
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  HAL_StatusTypeDef ret = HAL_OK;
+  
+  /*使能供电配置更新 */
+  MODIFY_REG(PWR->CR3, PWR_CR3_SCUEN, 0);
+
+  /* 当器件的时钟频率低于最大系统频率时，电压调节可以优化功耗，
+		 关于系统频率的电压调节值的更新可以参考产品数据手册。  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+ 
+  /* 启用HSE振荡器并使用HSE作为源激活PLL */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+  RCC_OscInitStruct.PLL.PLLM = 5;
+  RCC_OscInitStruct.PLL.PLLN = 160;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+ 
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+  
+	/* 选择PLL作为系统时钟源并配置总线时钟分频器 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK  | \
+																 RCC_CLOCKTYPE_HCLK    | \
+																 RCC_CLOCKTYPE_D1PCLK1 | \
+																 RCC_CLOCKTYPE_PCLK1   | \
+                                 RCC_CLOCKTYPE_PCLK2   | \
+																 RCC_CLOCKTYPE_D3PCLK1);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;  
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; 
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; 
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; 
+  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+  if(ret != HAL_OK)
+  {
+    while(1) { ; }
+  }
+}
+/****************************END OF FILE***************************/
